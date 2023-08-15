731d9f71d2e180c37715ab59fbabbf1c56e2e530
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-08-16 02:53:57 +0300

    Update build folder name to docs

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,11 @@ module.exports = {
     static: './dist',
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: './src/index.html'}),
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
   ],
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     clean: true,
   },
   module: {

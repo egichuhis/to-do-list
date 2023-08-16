0f18d455c7767866d6ec70381a6fa0f43188294f
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-08-16 11:52:35 +0300

    Add basic layout

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import './style.css';
-import refresh from './imgs/refresh.png';
+import refreshIconImg from './imgs/refresh.png';
+import returnIconImg from './imgs/diskette.png';
+import optionsIconImg from './imgs/options.png';
 
 const todoItems = [
   {
@@ -25,22 +27,46 @@ const todoItems = [
   },
 ];
 
-const listItem = () => {
+const listItems = () => {
   const listContainer = document.getElementById('list-container');
   const listTitleDiv = document.createElement('div');
+  const addToListDiv = document.createElement('div');
+  addToListDiv.id = 'add-to-list-div';
+  const addItemInput = document.createElement('input');
+  addItemInput.id = 'add-item-input';
+  addItemInput.type = 'text';
+  addItemInput.placeholder = 'Add to your list...';
+  const listItemsDiv = document.createElement('div');
   listTitleDiv.id = 'list-title-div';
   const listTitle = document.createElement('h4');
   listTitle.textContent = 'Today\'s To Do';
   const refreshIcon = document.createElement('img');
   refreshIcon.id = 'refresh-img';
-  refreshIcon.src = refresh;
+  refreshIcon.src = refreshIconImg;
+  const returnIcon = document.createElement('img');
+  returnIcon.id = 'return-img';
+  returnIcon.src = returnIconImg;
   const listItem = document.createElement('li');
-  listItem.innerHTML = 'List Item';
+  listItem.id = 'list-item';
+  const listItemCheck = document.createElement('input');
+  listItemCheck.type = 'checkbox';
+  const itemDescription = document.createElement('p');
+  itemDescription.textContent = 'Hello World, Micronauts are awesome!';
+  const optionsIcon = document.createElement('img');
+  optionsIcon.src = optionsIconImg;
+  optionsIcon.id = 'options-img';
 
   listTitleDiv.appendChild(listTitle);
   listTitleDiv.appendChild(refreshIcon);
+  addToListDiv.appendChild(addItemInput);
+  addToListDiv.appendChild(returnIcon);
   listContainer.appendChild(listTitleDiv);
-  listContainer.appendChild(listItem);
+  listContainer.appendChild(addToListDiv);
+  listContainer.appendChild(listItemsDiv);
+  listItemsDiv.appendChild(listItem);
+  listItem.appendChild(listItemCheck);
+  listItem.appendChild(itemDescription);
+  listItem.appendChild(optionsIcon);
 };
 
-listItem();
+listItems();

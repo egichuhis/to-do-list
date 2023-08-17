8f5b3b5ec8af3f1a3518dc4ef20f7bdb0a0065ca
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-08-17 03:54:49 +0300

    Add edit item feature

--- src/modules/generateList.js
+++ src/modules/generateList.js
@@ -9,13 +9,25 @@ const generateList = (items, listItemsDiv) => {
     const listItem = document.createElement('li');
     listItem.className = 'list-item';
     const listItemCheck = DOM.createCheckbox();
-    const itemDescription = DOM.createParagraph(item.description);
+    const descriptionInput = document.createElement('input');
+    descriptionInput.type = 'text';
+    descriptionInput.value = item.description;
+    descriptionInput.className = 'edit-input';
     const deleteIcon = DOM.createImage('options-img', deleteIconImg);
-    DOM.appendChildren(listItem, listItemCheck, itemDescription, deleteIcon);
+    DOM.appendChildren(listItem, listItemCheck, descriptionInput, deleteIcon);
     listItemsDiv.appendChild(listItem);
 
     deleteIcon.addEventListener('click', () => {
       removeItems(item.index - 1, items);
+      generateList(items, listItemsDiv);
+    });
+
+    descriptionInput.addEventListener('click', (event) => {
+      event.stopPropagation();
+    });
+
+    descriptionInput.addEventListener('blur', () => {
+      item.description = descriptionInput.value;
       generateList(items, listItemsDiv);
     });
   });

a7835b474bf999bec9c79d05b657eb71b7ba3999
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-08-24 13:12:06 +0300

    Remove items test working

--- addremove.test.js
+++ addremove.test.js
@@ -1,4 +1,5 @@
 const addItems = require('./src/modules/addItems.js');
+const removeItems = require('./src/modules/removeItems.js');
 
 // Mock the DOM environment
 const mockHtml = `
@@ -32,6 +33,32 @@ describe('addItems function', () => {
       const listItemElements = document.querySelectorAll;
 
       expect(listItemElements.length).toBe(1);
+    });
+  });
+});
+
+describe('removeItems function', () => {
+  test('should remove an <li> element from the list', () => {
+    const items = [
+      { description: 'Item 1', complete: false, index: 1 },
+      { description: 'Item 2', complete: false, index: 2 },
+    ];
+
+    const itemList = document.getElementById('item-list');
+    itemList.innerHTML = `
+      <li>Item 1</li>
+    `;
+
+    const removeItemsPromise = new Promise((resolve) => {
+      removeItems(1, items);
+      setTimeout(() => resolve(), 10);
+    });
+
+    return removeItemsPromise.then(() => {
+      const listItemElements = itemList.querySelectorAll('li');
+
+      expect(listItemElements.length).toBe(1);
+      expect(items[0].index).toBe(1);
     });
   });
 });

1d91ac744e920bb119c66e47e2c7f2f49e6e87c6
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-08-24 16:51:28 +0200

    Add empty completed.test.js file

--- clearCompleted.test.js
+++ clearCompleted.test.js
@@ -10,7 +10,6 @@ const clearCompleted = () => {
   generateList(filteredItems, listItemsDiv);
 };
 
-// Create a mock element with the necessary methods
 const mockElement = document.createElement('div');
 mockElement.setAttribute = jest.fn();
 
@@ -20,24 +19,18 @@ jest.mock('./src/modules/todoItems.js');
 
 describe('clearCompleted', () => {
   test('clears completed items and updates UI', () => {
-    // Mock data
     const mockItems = [
       { id: 1, description: 'Task 1', complete: true },
       { id: 2, description: 'Task 2', complete: false },
       { id: 3, description: 'Task 3', complete: true },
     ];
 
-    // Mock the getStoredTodoItems function to return mockItems
     getStoredTodoItems.mockReturnValue(mockItems);
 
-    // Call the clearCompleted function
     clearCompleted();
 
-    // Check if saveToLocalStorage was called with the filtered items
     expect(saveToLocalStorage).toHaveBeenCalledWith(
       mockItems.filter((item) => item.complete === false),
     );
   });
-
-  // Add more test cases as needed
 });
